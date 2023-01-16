6d1b20926cc3df051ae7fd04df6769396d94e5c9
Justinkr1 <justinkr@umich.edu>
2023-01-16 17:36:01 -0500

    Added files for bot
    added files for bot

---
+++ index.js
@@ -0,0 +1,59 @@
+require("dotenv").config()
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions } = require(`discord.js`);
+
+const prefix = '!';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.MessageContent] });
+
+client.on("ready", () => {
+    console.log("Bot is online!");
+
+    client.user.setActivity(`Big Bunga`, {type: "WATCHING" });
+
+})
+
+client.on("messageCreate", (message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    const command = args.shift().toLowerCase();
+
+    //message array
+
+    const messageArray = message.content.split(" ");
+    const argument = messageArray.slice(1);
+    const cmd = messageArray[0];
+
+    //COMMANDS
+
+// test command
+
+if (command === 'test') {
+    message.channel.send("Bot is working!")
+}
+
+
+
+
+
+
+
+
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
